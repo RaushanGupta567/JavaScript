3b44f2c5e8b01051d34febf2c5ce40ba86a78930
RaushanGupta567 <raushan.qa.vtu@gmail.com>
2026-07-10 14:35:50 +0000

    String

---
+++ 01_basic/String.js
@@ -0,0 +1,40 @@
+const name = "Raushan"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const number = new Number(20)
+console.log(number)
+console.log(number.toString())
+console.log(typeof number)
+
+const gameName = new String('Raushan-hc-com')
+
+// console.log(gameName[0]);
+//  console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('u'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+// const newStringOne = "   hitesh    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://hitesh.com/hitesh%20choudhary"
+
+// console.log(url.replace('%20', '-'))
+
+// console.log(url.includes('sundar'))
+
+// console.log(gameName.split('-'));
